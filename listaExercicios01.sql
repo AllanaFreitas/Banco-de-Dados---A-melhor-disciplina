bc511d681b6d721ac6e3a3bfb02548ff28221fd5
AllanaFreitas <fallana807@gmail.com>
2023-09-05 11:33:48 -0300

    atividade 4

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -3,3 +3,5 @@ SELECT titulo FROM livros;
 SELECT nome_autor FROM autores WHERE ano_nascimento < 1900;
 
 SELECT titulo FROM livros WHERE autor = 'J.K. Rowling';
+
+SELECT nome_aluno FROM alunos WHERE curso = 'Engenharia de Software';
