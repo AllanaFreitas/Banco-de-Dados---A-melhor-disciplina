9e5a49523b77c292728bfa75e66818ff45342c2e
AllanaFreitas <fallana807@gmail.com>
2023-09-26 09:34:38 -0300

    Lista 2 - EX3

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -30,3 +30,24 @@ CALL sp_LivrosPorCategoria('Ciência');
 CALL sp_LivrosPorCategoria('Ficção Científica');
 CALL sp_LivrosPorCategoria('História');
 CALL sp_LivrosPorCategoria('Autoajuda');
+
+/* 3 */
+
+DELIMITER //
+
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN nome_da_categoria VARCHAR(200), OUT total_livros INT)
+BEGIN
+SELECT COUNT(*) INTO total_livros
+FROM Livro
+INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+WHERE Categoria.Nome = nome_da_categoria;
+END;
+//
+
+DELIMITER ;
+
+CALL sp_ContarLivrosPorCategoria('Romance', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Ciência', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Ficção Científica', @total_livros);
+CALL sp_ContarLivrosPorCategoria('História', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Autoajuda', @total_livros);
