18606a7de6b0fa1d4c56c043d33f03f29e55fb5b
AllanaFreitas <fallana807@gmail.com>
2023-09-26 09:37:42 -0300

    Lista 2 - EX7

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -140,3 +140,39 @@ END;
 DELIMITER ;
 
 CALL sp_TitulosPorCategoria('Romance');
+
+/* 7 */
+
+DELIMITER //
+
+CREATE PROCEDURE sp_AdicionarLivro(
+IN titulo_livro VARCHAR(255),
+IN editora_id INT,
+IN ano_publicacao INT,
+IN numero_paginas INT,
+IN categoria_id INT
+)
+BEGIN
+
+
+IF titulo_do_livro IS NULL OR editora_id IS NULL OR ano_publicacao IS NULL OR numero_paginas IS NULL OR categoria_id IS NULL THEN
+    SELECT 'Erro: Nenhum dos parâmetros pode ser nulo.' AS Status;
+ELSE
+    IF EXISTS (SELECT 1 FROM livro WHERE Titulo = titulo_do_livro) THEN
+        SELECT 'Erro: O livro com esse título já existe na tabela.' AS Status;
+    ELSE
+        INSERT INTO Livro (Titulo, Editora_ID, Ano_Publicacao, Numero_Paginas, Categoria_ID)
+        VALUES (titulo_livro, editora_id, ano_publicacao, numero_paginas, categoria_id);
+
+        SELECT 'Livro adicionado com sucesso.' AS Status;
+    END IF;
+END IF;
+
+
+
+END;
+//
+
+DELIMITER ;
+
+CALL sp_AdicionarLivro('É assim que acaba', 1, 2016, 374, 1);
